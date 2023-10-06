8000f82b9e7071c6151a20d9cd42f77ebb0489a3
linux <narendrac024@gmail.com>
2023-10-06 09:54:40 +0530

    All Task Done

--- index.js
+++ index.js
@@ -5,77 +5,84 @@ const _ = require('lodash');
 const app = express();
 const port = 3000;
 
-let blogData; // Initialize the blogData variable outside the functions
+// Initialize caches for analytics and search
+const analyticsCache = new Map();
+const searchCache = new Map();
 
-// Middleware to fetch and analyze blog data
-const fetchAndAnalyzeBlogData = async () => {
+// Function to fetch blog data
+const fetchData = async () => {
     try {
-        // Make a GET request to the third-party API
         const response = await axios.get('https://intent-kit-16.hasura.app/api/rest/blogs', {
             headers: {
                 'x-hasura-admin-secret': '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6',
             },
         });
 
-        blogData = response.data; // Update the blogData variable with API response
-
-        // Calculate total number of blogs
-        const totalBlogs = blogData.length;
-
-        // Find the blog with the longest title
-        const longestTitleBlog = _.maxBy(blogData, (blog) => blog.title.length);
-
-        // Determine the number of blogs with titles containing "privacy"
-        const privacyBlogs = _.filter(blogData, (blog) =>
-            blog.title.toLowerCase().includes('privacy')
-        );
-
-        // Create an array of unique blog titles
-        const uniqueTitles = _.uniqBy(blogData, 'title').map((blog) => blog.title);
-
-        // Respond with the statistics
-        return {
-            totalBlogs,
-            longestTitle: longestTitleBlog.title,
-            privacyBlogs: privacyBlogs.length,
-            uniqueTitles,
-        };
+        return response.data.blogs;
     } catch (error) {
-        // Handle errors
-        console.error('Error fetching or analyzing blog data:', error);
+        console.error('Error fetching blog data:', error);
         throw error;
     }
 };
 
-// Apply memoization to the function with a cache duration of 5 minutes (300,000 milliseconds)
-const memoizedFetchAndAnalyze = _.memoize(fetchAndAnalyzeBlogData, null, 300000);
+// Function to get analytics data with caching
+const getAnalytics = async () => {
+    if (analyticsCache.has('data')) {
+        return analyticsCache.get('data');
+    }
 
-// Route for fetching blog statistics
+    const blogData = await fetchData();
+
+    const totalBlogs = blogData.length;
+    const longestTitleBlog = _.maxBy(blogData, (blog) => blog.title.length);
+    const privacyBlogsCount = blogData.filter((blog) => blog.title.toLowerCase().includes('privacy')).length;
+    const uniqueTitles = _.uniqBy(blogData, 'title').map((blog) => blog.title);
+
+    const analyticsData = {
+        totalBlogs,
+        longestTitle: longestTitleBlog.title,
+        privacyBlogsCount,
+        uniqueTitles,
+    };
+
+    analyticsCache.set('data', analyticsData, 300000); // Cache for 5 minutes (300,000 milliseconds)
+    return analyticsData;
+};
+
+// Function to get search results with caching
+const getSearchResults = async (query) => {
+    const cacheKey = `search:${query}`;
+
+    if (searchCache.has(cacheKey)) {
+        return searchCache.get(cacheKey);
+    }
+
+    const blogData = await fetchData();
+
+    const matchingBlogs = blogData.filter((blog) => blog.title.toLowerCase().includes(query));
+    searchCache.set(cacheKey, matchingBlogs, 300000); // Cache for 5 minutes (300,000 milliseconds)
+    return matchingBlogs;
+};
+
+// API routes
 app.get('/api/blog-stats', async (req, res) => {
     try {
-        const statistics = await memoizedFetchAndAnalyze();
+        const statistics = await getAnalytics();
         res.json(statistics);
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
-// Route for searching blogs
-app.get('/api/blog-search', (req, res) => {
+app.get('/api/blog-search', async (req, res) => {
     const query = req.query.query.toLowerCase();
 
-    // Filter blogs based on the provided query (case-insensitive)
-    const matchingBlogs = blogData.filter((blog) =>
-        blog.title.toLowerCase().includes(query)
-    );
-
-    res.json(matchingBlogs);
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error('Error:', err.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    try {
+        const matchingBlogs = await getSearchResults(query);
+        res.json(matchingBlogs);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 app.listen(port, () => {
